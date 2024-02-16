9d8250d76062ebbdf6efdcc0d307a25c3ebd186f
Albert Avanesyan <117994805+abavanesyan@users.noreply.github.com>
2024-02-16 23:21:51 +0400

    Add files via upload

---
+++ Task 7.cs
@@ -0,0 +1,108 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Give the value of row");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Give the value of column");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[8, 8];
+List<int[]> loc = new List<int[]>();
+int maxindex = 0;
+int maxcounter = 0;
+void helper(int n, int m, int index) // This function is for finding the best place that the queen can be placed
+{
+    int counter = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (n - 1 == i && m - 1 == j)
+            {
+            }
+            else if ((Math.Abs(n - 1 - i) == Math.Abs(m - 1 - j)) || j == m - 1 || i == n - 1)
+            {
+            }
+            else
+            {
+                if (arr[i, j] != 1 && arr[i, j] != 9)
+                {
+                    counter++;
+                }
+            }
+        }
+    }
+    if (counter > maxcounter)
+    {
+        maxcounter = counter;
+        maxindex = index;
+    }
+}
+void queen(int n, int m)
+{
+    maxindex = 0;
+    maxcounter = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (n - 1 == i && m - 1 == j)
+            {
+                arr[i, j] = 9;
+            }
+            else if ((Math.Abs(n - 1 - i) == Math.Abs(m - 1 - j)) || j == m - 1 || i == n - 1)
+            {
+                arr[i, j] = 1;
+            }
+        }
+    }
+    loc.Clear();
+    for (int k = 0; k < 8; k++)
+    {
+        for (int l = 0; l < 8; l++)
+        {
+            if (arr[k, l] == 9)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Q ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (arr[k, l] == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("1 ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                loc.Add([k, l]);
+                Console.Write("0 ");
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("Do you want to continue (y or n)");
+    string ans = Console.ReadLine();
+
+    if (ans == "y")
+    {
+        if (loc.Count != 0)
+        {
+            for (int i = 0; i < loc.Count; i++)
+            {
+                helper(loc[i][0] + 1, loc[i][1] + 1, i);
+            }
+            queen(loc[maxindex][0] + 1, loc[maxindex][1] + 1);
+        }
+        else
+        {
+            Console.WriteLine("There is no place left");
+        }
+    }
+}
+if (n > 0 && n < 9 && m > 0 && m < 9)
+{
+    queen(n, m);
+}
+else
+{
+    Console.WriteLine("You have mistaken the numbers");
+}
---
+++ Task 8.cs
@@ -0,0 +1,81 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Give the value of row");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Give the value of column");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = { {2, 3, 4, 4, 4, 4, 3, 2 },
+               {3, 4, 6, 6, 6, 6, 4, 3 },
+               {4, 6, 8, 8, 8, 8, 6, 4 },
+               {4, 6, 8, 8, 8, 8, 6, 4 },
+               {4, 6, 8, 8, 8, 8, 6, 4 },
+               {4, 6, 8, 8, 8, 8, 6, 4 },
+               {3, 4, 6, 6, 6, 6, 4, 3 },
+               {2, 3, 4, 4, 4, 4, 3, 2 }, };
+
+int min, ind_i, ind_j;
+void knight(int n, int m)
+{
+    min = 9;
+    ind_i = 0;
+    ind_j = 0;
+    for (int i = 1; i < 9; i++)
+    {
+        for (int j = 1; j < 9; j++)
+        {
+            if (arr[i - 1, j - 1] == 9)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("K ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if ((Math.Abs(n - i) == 2 && Math.Abs(m - j) == 1) || (Math.Abs(n - i) == 1 && Math.Abs(m - j) == 2))
+            {
+                if (arr[i - 1, j - 1] < min)
+                {
+                    min = arr[i - 1, j - 1];
+                    ind_i = i;
+                    ind_j = j;
+                }
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(arr[i - 1, j - 1] + " ");
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+            else if (n == i && m == j)
+            {
+                arr[i - 1, j - 1] = 9;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("K ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.Write("0 ");
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("Do you want to continue (y or n)");
+    string ans = Console.ReadLine();
+
+    if (ans == "y")
+    {
+        if (min != 9)
+        {
+            knight(ind_i, ind_j);
+        }
+        else
+        {
+            Console.WriteLine("There is no place left");
+        }
+    }
+}
+if (n > 0 && n < 9 && m > 0 && m < 9)
+{
+    knight(n, m);
+}
+else
+{
+    Console.WriteLine("You have mistaken the numbers");
+}
