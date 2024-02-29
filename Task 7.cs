aaab23056058efa67c1162240c832ee89ed36b45
Albert Avanesyan <117994805+abavanesyan@users.noreply.github.com>
2024-02-29 23:04:58 +0400

    Task 7.cs

--- Task 7.cs
+++ Task 7.cs
@@ -1,10 +1,11 @@
-﻿Console.ForegroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine("Give the value of row");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Give the value of column");
 int m = Convert.ToInt32(Console.ReadLine());
 
 int[,] arr = new int[8, 8];
+int[,] arr2 = new int[8, 8];
 List<int[]> loc = new List<int[]>();
 int maxindex = 0;
 int maxcounter = 0;
@@ -30,6 +31,7 @@ void helper(int n, int m, int index) // This function is for finding the best pl
             }
         }
     }
+    arr2[n - 1, m - 1] = counter;
     if (counter > maxcounter)
     {
         maxcounter = counter;
@@ -90,6 +92,42 @@ void queen(int n, int m)
             {
                 helper(loc[i][0] + 1, loc[i][1] + 1, i);
             }
+            for (int k = 0; k < 8; k++)
+            {
+                for (int l = 0; l < 8; l++)
+                {
+                    if (arr[k, l] == 1)
+                    {
+                        Console.Write("1  ");
+                    }
+                    else if (arr[k, l] == 9)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Q  ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (arr2[k, l] == maxcounter)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(maxcounter + " ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        if (maxcounter < 10)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    else if (arr2[k, l] >= 0)
+                    {
+                        Console.Write(arr2[k, l] + " ");
+                        if (arr2[k, l] < 10)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
             queen(loc[maxindex][0] + 1, loc[maxindex][1] + 1);
         }
         else
